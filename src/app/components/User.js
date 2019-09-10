50a1854d76e8bca02b563060fe05d6501825374e
NKV-88 <sysadm86@gmail.com>
2019-09-10 17:00:50 +0500

    Дома продолжить!

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class User extends Component {
     render() {
         return (
             <div className="card border-secondary mb-3">
-                <div className="card-header">{this.props.username}</div>
+                <div className="card-header">
+                    <Link to={`/users/${this.props.id}`}>{this.props.username}</Link>
+                </div>
                 <div className="card-body text-secondary">
                     <p>{this.props.name}</p>
                     <p>{this.props.email}</p>
--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ import { Router, Route, IndexRoute, browserHistory} from 'react-router'
 import Layout from './app/layouts/Layout';
 import Main from './app/pages/Main';
 import Users from './app/pages/Users';
+import User from './app/pages/User';
 import PageNotFound from './app/pages/PageNotFound'
 
 ReactDOM.render (
     <Router history={browserHistory}>
         <Route path="/" component={Layout}>
             <IndexRoute component ={Main}/>
-            <Route path="users" component={Users}/>
+            <Route path="users" component={Users}>
+                <Route path=":userId" component={User}/>
+            </Route>
             <Route path="*" component={PageNotFound}/>
         </Route>
     </Router>,

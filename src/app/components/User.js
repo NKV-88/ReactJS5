17b3998fe9ef37104ae668c72e2528871bfc77af
NKV-88 <sysadm86@gmail.com>
2019-09-11 08:34:53 +0500

    Оптимизировали код

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -3,17 +3,19 @@ import { Link } from 'react-router';
 
 class User extends Component {
     render() {
+        const {id, name, email, phone, website, username } = this.props;
+
         return (
             <div className="card border-secondary mb-3">
                 <div className="card-header">
-                    <Link to={`/users/${this.props.id}`}>{this.props.username}</Link>
+                    <Link to={`/users/${id}`}>{username}</Link>
                 </div>
                 <div className="card-body text-secondary">
-                    <p>{this.props.name}</p>
-                    <p>{this.props.email}</p>
-                    <p>{this.props.phone}</p>
-                    <p>{this.props.website}</p>
-                </div>                
+                    <p>{name}</p>
+                    <p>{email}</p>
+                    <p>{phone}</p>
+                    <p>{website}</p>
+                </div>     
             </div>
         );
     }
